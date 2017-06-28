724d38c933aacdab612dd9a41fbde5b22522a315
Jordan Benner <jordanbenner@outlook.com>
2017-06-28 14:56:20 -0500

    - constructor
    Got rid of constructor for square

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
-class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: null
-    };
-  }
+// class Square extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       value: null
+//     };
+//   }
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
   clickedBox () {
     console.log('Box', this.props.boxNum);
@@ -24,12 +32,28 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({squares:squares, xIsNext: !this.state.xIsNext});
+  }
+
   renderSquare(i) {
-    return <Square boxNum={i}/>
+    return <Square boxNum={this.state.squares[i]}
+    onClick={() => this.handleClick(i)}/>
+
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
