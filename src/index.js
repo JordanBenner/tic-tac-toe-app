592de9dbad1be4f823f2f52c979839ec604aed5c
Jordan Benner <jordanbenner@outlook.com>
2017-06-28 13:59:24 -0500

    Broken div
    Broken div for rendering blocks

--- src/index.js
+++ src/index.js
@@ -1,60 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './app';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      value: null,
-    };
-  }
 
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.props.value}
-      </button>
-    );
-  }
 
-}
-
-class Board extends React.Component {
-  renderSquare(i){
-    return <Square/>
-  }
-
-  render () {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className='status'>{status}</div>
-        <div className='board-row'>
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
-      </div>
-      <div className='board-row'>
-        {this.renderSquare(3)}
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
-      </div>
-      <div className='board-row'>
-        {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
-      </div>
-
-    );
-  }
-}
-
-class Game extends React.Component {
-  render () {
-    return <Board/>
-  }
-}
-
-ReactDom.render(<Game/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
