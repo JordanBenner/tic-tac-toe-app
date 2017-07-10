5db9ba70a4769a81e62468afb3a0c7ebb2523baf
Jordan Benner <jordanbenner@outlook.com>
2017-07-09 19:07:12 -0500

    Changed method in square con
    Added props

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-// class Square extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       value: null
-//     };
-//   }
+class Square extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: null
+    };
+  }
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
+// function Square(props) {
+//   return (
+//     <button className="square" onClick={props.onClick}>
+//       {props.value}
+//     </button>
+//   );
+// }
 
   clickedBox () {
     console.log('Box', this.props.boxNum);
@@ -42,6 +42,9 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({squares:squares, xIsNext: !this.state.xIsNext});
   }
@@ -84,25 +87,7 @@ class Board extends React.Component {
   }
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+
 
 class Game extends React.Component {
   render() {
